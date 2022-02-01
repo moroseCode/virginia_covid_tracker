7d8e7e41613b72685c9461813bb1fed118f13ee7
digitalsamsquanch <digital.samsquanch@protonmail.com>
2022-01-31 21:41:15 -0500

    everything working but the resetting of the button when the data is reset to the default set

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="grid md:grid-cols-3 gap-4">
+    <!-- Box 1 -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        Pending...
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(stats.total_cases) }}
+      </div>
+    </div>
+    <!-- Box 2 -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Hospitalizations</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        Pending...
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(stats.hospitalizations) }}
+      </div>
+    </div>
+    <!-- Box 3 -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        Pending...
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(stats.deaths) }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataBoxes',
+  props: ['stats'],
+  methods: {
+    numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/LocalitySelect.vue
@@ -0,0 +1,38 @@
+<template>
+  <select
+    @change="onChange"
+    v-model="selected"
+    class="form-select mt-10 block w-full border p-3 rounded"
+  >
+    <option value="0">Select Locality</option>
+    <option
+      v-for="locality in resultData"
+      :value="locality.fips"
+      :key="locality.fips"
+    >
+      {{ locality.locality }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'LocalitySelect',
+  props: ['resultData'],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      const place = this.resultData.find((item) => item.fips === this.selected);
+
+      console.log(place);
+      this.$emit('get-locality', place);
+    },
+  },
+};
+</script>
+
+<style></style>
