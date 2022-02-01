bdd2c2ce4d2377a7e98f1fe7f5e367d1cfd6c702
digitalsamsquanch <digital.samsquanch@protonmail.com>
2022-01-31 21:41:15 -0500

    everything working but the resetting of the button when the data is reset to the default set

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,92 @@
 <template>
   <div class="home">
-    Virginia COVID Cases
-    {{ totalCases }}
+    <main v-if="!loading">
+      <DataTitle :text="title" :dataDate="dataDate" />
+      <DataBoxes :stats="stats" />
+      <LocalitySelect
+        @get-locality="getLocalityData"
+        :resultData="resultData"
+      />
+      <button
+        @click="clearLocalityData"
+        v-if="stats.locality"
+        class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+      >
+        Clear Locality
+      </button>
+    </main>
+    <main class="flex flex-col align-center justify-center text-center" v-else>
+      <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+      <img :src="loadingImage" class="w-24 m-auto" alt="" />
+    </main>
   </div>
 </template>
 
 <script>
-// import previousFriday from 'date-fns/previousFriday'
-import { format, isWeekend, previousFriday, parseISO } from 'date-fns';
+import { format, isWeekend, previousFriday } from 'date-fns';
+import DataTitle from '@/components/DataTitle';
+import DataBoxes from '@/components/DataBoxes';
+import LocalitySelect from '@/components/LocalitySelect';
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    DataTitle,
+    DataBoxes,
+    LocalitySelect,
+  },
   methods: {
     async fetchCovidData() {
-      let today = format(new Date(), 'yyyy-MM-dd');
-      today = parseISO(today);
+      let today = new Date();
       console.log(today);
       console.log(isWeekend(today));
-      console.log(previousFriday(today));
       if (isWeekend(today)) {
-        return today === previousFriday(today);
+        today = previousFriday(today);
       }
-      format(today, 'yyyy-MM-dd');
+      let dateString = format(today, 'yyyy-MM-dd');
+      dateString = '2022-01-28';
       const res = await fetch(
-        `https://data.virginia.gov/resource/bre9-aqqr.json?report_date=2022-01-28&$$app_token=S9lLLe9oUwwchcETbYZdOsPvO`
+        `https://data.virginia.gov/resource/bre9-aqqr.json?report_date=${dateString}&$$app_token=S9lLLe9oUwwchcETbYZdOsPvO`
       );
-      console.log(res);
-      const data = res.json();
-      console.log(data);
+
+      const data = await res.json();
       return data;
+    },
+    getLocalityData(locality) {
+      this.stats = locality;
+      this.title = locality.locality;
+    },
+    clearLocalityData() {
+      this.loading = true;
+      this.title = 'Virginia';
+      this.stats.total_cases = this.totalCases;
+      this.stats.hospitalizations = this.totalHospitalizations;
+      this.stats.deaths = this.totalDeaths;
+      this.loading = false;
     },
   },
   data() {
     return {
+      loading: true,
+      title: 'Virginia',
+      dataDate: '',
+      stats: {
+        total_cases: null,
+        deaths: null,
+        hospitalizations: null,
+      },
+      localities: [],
       resultData: [],
+      loadingImage: require('../assets/hourglass.gif'),
     };
   },
   async created() {
     this.resultData = await this.fetchCovidData();
+    this.stats.total_cases = this.totalCases;
+    this.stats.hospitalizations = this.totalHospitalizations;
+    this.stats.deaths = this.totalDeaths;
+    this.dataDate = this.resultData.report_date;
+    this.localities = this.all_localities;
+    this.loading = false;
   },
   computed: {
     totalCases() {
@@ -47,7 +96,29 @@ export default {
       }
       console.log(total);
       return total;
-      // 11,160,816
+    },
+    totalDeaths() {
+      let total = 0;
+      for (const { deaths } of this.resultData) {
+        total += parseInt(deaths);
+      }
+      console.log(total);
+      return total;
+    },
+    totalHospitalizations() {
+      let total = 0;
+      for (const { hospitalizations } of this.resultData) {
+        total += parseInt(hospitalizations);
+      }
+      console.log(total);
+      return total;
+    },
+    all_localities() {
+      let places = [];
+      for (const { locality } of this.resultData) {
+        places.push(locality);
+      }
+      return places;
     },
   },
 };
